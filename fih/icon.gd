8acb114da81654a94d2c6394b1db21effb8c7115
Pietske-Pieling <45796215+Pietske-Pieling@users.noreply.github.com>
2026-06-19 20:57:44 +0200

    Spiel gedroppt

---
+++ fih/icon.gd
@@ -0,0 +1,41 @@
+extends Sprite2D
+var speed = 30
+var delta = 20
+
+var row = 0
+
+# Hier laden wir die Bild-Szene, die wir oben erstellt haben
+@export var taurus: PackedScene = preload("res://taurus.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	# Nach oben bewegen (Grenze ist 0)
+	if Input.is_action_just_pressed("ui_down"):
+		if row < 5:
+			row += 1
+
+	# Nach unten bewegen (Grenze ist die Viewport-Höhe .y)
+	if Input.is_action_just_pressed("ui_up"):
+		if row > 0:
+			row -= 1
+	
+	if Input.is_action_just_pressed("fire_taurus"):
+
+		var new_taurus = taurus.instantiate()
+		
+		# 2. Dem Bild die aktuelle Position des Spawners geben
+		new_taurus.global_position = global_position
+		
+		new_taurus.global_position.y = new_taurus.global_position.y
+		
+		# 3. Das Bild zur Hauptszene hinzufügen
+		get_parent().add_child(new_taurus)
+	
+		
+	position.y = row * 180 + 200
+	pass
