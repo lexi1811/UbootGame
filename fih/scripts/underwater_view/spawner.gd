a75792a8cced31a14c3be63564c58b6bf0a0dedc
Jannehaw <wko542@haw-hamburg.de>
2026-06-20 21:55:18 +0200

    kraken wird gekocht
    aber noch nicht fertig

--- fih/scripts/underwater_view/spawner.gd
+++ fih/scripts/underwater_view/spawner.gd
@@ -1,27 +1,30 @@
 extends Node2D
 
-# Hier laden wir die Bild-Szene, die wir oben erstellt haben
 @export var image_scene: PackedScene = preload("res://scenes/underwater_view/hamburger_wale.tscn")
-
 @export var new_scene: PackedScene = preload("res://scenes/underwater_view/shark.tscn")
-
+@export var kraken_scene: PackedScene = preload("res://scenes/underwater_view/kraken.tscn")
 @export var temp_scene: PackedScene = preload("res://scenes/underwater_view/shark.tscn")
+
 func _on_timer_timeout() -> void:
-	var random_int = randi_range(1, 5)
-	var random_enemy = randi_range(1,2)
+	if get_tree().get_nodes_in_group("kraken").size() > 0:
+		return
+
+	var roll = randf()
+	var is_kraken = false
 	
-	if random_enemy == 1:
+	if roll < 0.05:
+		temp_scene = kraken_scene
+		is_kraken = true
+	elif roll < 0.30:
 		temp_scene = new_scene
-	elif random_enemy == 2:
+	else:
 		temp_scene = image_scene
 	
-	# 1. Eine neue Instanz des Bildes erstellen
 	var new_image = temp_scene.instantiate()
-	
-	# 2. Dem Bild die aktuelle Position des Spawners geben
 	new_image.global_position = global_position
 	
-	new_image.global_position.y = new_image.global_position.y + 180 * random_int
-	
-	# 3. Das Bild zur Hauptszene hinzufügen
+	if not is_kraken:
+		var random_int = randi_range(1, 5)
+		new_image.global_position.y = new_image.global_position.y + 180 * random_int
+		
 	get_parent().add_child(new_image)
