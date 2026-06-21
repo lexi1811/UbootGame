1f400dc6c263ee67f68ff7ba5079c5b9f9b02a1a
Jorik <otto.jorik@gmail.com>
2026-06-21 14:06:23 +0200

    update

--- fih/scripts/underwater_view/taurus.gd
+++ fih/scripts/underwater_view/taurus.gd
@@ -27,10 +27,11 @@ func _physics_process(delta: float) -> void:
 		elif hit_object.is_in_group("rightBorder"):
 			queue_free()
 		
-		var new_explosion = explo_scene.instantiate()
-		new_explosion.global_position = global_position
-		new_explosion.global_position.x += 200
-		get_parent().add_child(new_explosion)
-		get_tree().create_timer(0.7).timeout.connect(new_explosion.queue_free)
+		if !hit_object.is_in_group("kraken"):
+			var new_explosion = explo_scene.instantiate()
+			new_explosion.global_position = global_position
+			new_explosion.global_position.x += 200
+			get_parent().add_child(new_explosion)
+			get_tree().create_timer(0.7).timeout.connect(new_explosion.queue_free)
 		
 		queue_free()
