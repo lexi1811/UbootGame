5d32c1079ce4c02e4a9817924a318f8977529cba
Pietske-Pieling <45796215+Pietske-Pieling@users.noreply.github.com>
2026-06-21 14:01:28 +0200

    added sound

--- fih/scenes/game_over/label_highscore.gd
+++ fih/scenes/game_over/label_highscore.gd
@@ -3,3 +3,6 @@ extends Label
 func _ready() -> void:
 	# Da das Skript selbst ein Label ist, änderst du hier direkt seinen eigenen Text:
 	text = "Punkte: " + str(game_state.pointsSum)
+	var sfx = preload("res://assets/audio/sfx/Sad Trombone.mp3")
+	$AudioStreamPlayer2D.stream = sfx
+	$AudioStreamPlayer2D.play()
