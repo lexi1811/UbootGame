22b84cf8dd2102f8a07a51a36c2f84e0faf9948e
Pietske-Pieling <45796215+Pietske-Pieling@users.noreply.github.com>
2026-06-21 15:30:23 +0200

    added wsp and hwale und hab den engineer größer gemacht

--- fih/main_menu.gd
+++ fih/main_menu.gd
@@ -6,9 +6,5 @@ func _on_start_pressed() -> void:
 func _on_how_to_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/how_to_play/how_to_play.tscn")	
 
-
-func _on_settings_pressed() -> void:
-	print("Einstellungen geklickt!")
-
 func _on_exit_pressed() -> void:
 	get_tree().quit()
---
+++ fih/scenes/submarine_view/nachladen.gd
@@ -0,0 +1,19 @@
+extends Sprite2D
+
+func _ready() -> void:
+	make_green()
+
+func _process(delta: float) -> void:
+	if game_state.amunition <= 0:
+		make_red()
+	else:
+		make_green()
+
+
+# Funktion für Grün (System läuft)
+func make_green():
+	self.modulate = Color.from_string("#2ecc71", Color.GREEN)
+
+# Funktion für Rot (System zerstört!)
+func make_red():
+	self.modulate = Color.from_string("#ff4d4d", Color.RED)
