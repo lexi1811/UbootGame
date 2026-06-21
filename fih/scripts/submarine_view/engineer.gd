3f9d3b99a72ba555054dff73c50c31939f8e8309
Pietske-Pieling <45796215+Pietske-Pieling@users.noreply.github.com>
2026-06-21 13:39:47 +0200

    musik

--- fih/scripts/submarine_view/engineer.gd
+++ fih/scripts/submarine_view/engineer.gd
@@ -34,6 +34,13 @@ func get_input():
 	if input_dir != Vector2.ZERO:
 		player_moved = true
 		$ProgressBar.value = 0
+		if !$AudioStreamPlayer2D.is_playing():
+			var random_int = randi_range(1, 3)
+			var sfx_path = "res://assets/audio/sfx/engineer-step-" + str(random_int) + ".ogg"
+			var sfx = load(sfx_path)
+			
+			$AudioStreamPlayer2D.stream = sfx
+			$AudioStreamPlayer2D.play()
 	else:
 		player_moved = false
 
@@ -43,14 +50,20 @@ func _physics_process(_delta):
 	
 func _process(delta: float) -> void:
 	# Sicherheitshalber prüfen wir hier auch, ob der Spieler überhaupt in einem Raum ist
-	if Input.is_action_pressed("player_repair") && !player_moved && current_room != rooms.NONE:
-		
+	if Input.is_action_pressed("player_repair") && !player_moved:
+		var sfx_repair = preload("res://assets/audio/sfx/repair.mp3")
+		var sfx_reload = preload("res://assets/audio/sfx/reload.mp3")
+		if !$AudioStreamPlayer2D.is_playing():
+			if current_room == rooms.RELOAD:
+				$AudioStreamPlayer2D.stream = sfx_reload
+			else:
+				$AudioStreamPlayer2D.stream = sfx_repair
+			$AudioStreamPlayer2D.play()
 		# HIER NEU: Wir holen uns die passende Geschwindigkeit für den aktuellen Raum
 		var current_speed = room_repair_speeds[current_room]
 		$ProgressBar.value += current_speed * delta
 		
 		if $ProgressBar.value >= $ProgressBar.max_value:
-			print("Repariert im Raum: ", rooms.keys()[current_room])
 			if current_room == rooms.ENGINE:
 				game_state._fix_system(global_enums.System.ENGINE)
 			elif current_room == rooms.SONAR:
@@ -66,6 +79,7 @@ func _process(delta: float) -> void:
 			$ProgressBar.value = 0
 	else:
 		$ProgressBar.value -= 20 * delta
+		$AudioStreamPlayer2D.stop()
 
 # --- HIER SIND DEINE ENUM-FUNKTIONEN ---
 
